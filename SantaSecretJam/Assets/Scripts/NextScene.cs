ef5381f3fa128d76b672a471b2a342bf4714e8f5
ThiagoPeriera232 <pereira.tp.thiago@gmail.com>
2021-12-15 14:38:58 -0300

    transição entre cenarios ok - efeito, milly, player

--- SantaSecretJam/Assets/Scripts/NextScene.cs
+++ SantaSecretJam/Assets/Scripts/NextScene.cs
@@ -10,10 +10,11 @@ public class NextScene : MonoBehaviour
     [SerializeField] DestinationIdentifier destinationPortal;
     [SerializeField] Transform spawnPoint;
     private Player player;
+    private Milly milly;
 
-    private void Start()
-    {
-    }
+    public Animator transition;
+    public float transitionTime = 1f;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
@@ -25,11 +26,25 @@ public class NextScene : MonoBehaviour
     IEnumerator Teleport()
     {
         DontDestroyOnLoad(gameObject);
+
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
         yield return SceneManager.LoadSceneAsync(sceneName);
-        player = FindObjectOfType<Player>();
+
+        FindObjects();
+
         var destPortal = FindObjectsOfType<NextScene>().First(x => x != this && x.destinationPortal == this.destinationPortal);
         player.transform.position = new Vector2(destPortal.spawnPoint.position.x, destPortal.spawnPoint.position.y);
+        milly.transform.position = new Vector2(player.transform.position.x + 1f, player.transform.position.y + 1f);
+
         Destroy(gameObject);
+    }
+
+    void FindObjects()
+    {
+        player = FindObjectOfType<Player>();
+        milly = FindObjectOfType<Milly>();
     }
 }
 
